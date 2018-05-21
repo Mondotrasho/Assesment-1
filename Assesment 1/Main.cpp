c5550f4327dd849c901921a2909eb5a029c8087a
Mondotrasho <oscar@peoplepictures.com.au>
2018-05-21 14:18:39 +1000

    REMOVED CHUNK READY TO MERGE WITH GRID ARRAYS CODE

--- Assesment 1/Main.cpp
+++ Assesment 1/Main.cpp
@@ -6,52 +6,14 @@
 #include <windows.h>
 using namespace std;
 
-
-//
-
-
-//############ KEY CODES ##############
-#define KEY_UP 72 //or 224 72
-#define KEY_DOWN 80 //or 224 80
-#define KEY_LEFT 75 //or 224 75
-#define KEY_RIGHT 77 //or 224 77
-#define KEY_ENTER 13
-//####### Allow getting input #########
-
 void gravity(bool playerCurrentLayer[])
 {
 
 }
 
+
 int main()
 {
-	//clock_t
-
-
-	int key = 0; //current key pick with getch() ie c = getch() will make c = 72 if the up key is pressed
-
-	bool playerCurrentLayer[3] = { 0,0,1 };
-	bool destinationLayer[3] = { 0,0,0 };
-	//################################## MAP ARRAYS ####################################################
-	bool position[3][41] = {//where the player is currently
-		{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},//sky
-		{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},//middle
-		{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0} };//ground
-/*
-	colission_Func(player.x, player, y)
-
-		if(collision[player.y][player.x] == 1)
-		{
-			update_position()
-		}
-		*/
-
-	bool collision[3][41] = { //grid of true tiles you can move into and false tiles you cannot
-		{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},//sky
-		{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1},//middle
-		{1,0,1,1,1,1,1,0,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,0,0,1,1,1,1,1,1,1,1,1,1} };//ground
-
-	//###################################################################################################
 	//Mobs VVV
 	string player = "o";
 	//mapVVVV
@@ -63,18 +25,6 @@ int main()
 	cout << sky << endl << middle << endl << ground << endl; //prints empty map
 	
 	//arrows work
-	while (1) {
-		int left = GetAsyncKeyState(VK_LEFT);
-		int right = GetAsyncKeyState(VK_RIGHT);
-		int up = GetAsyncKeyState(VK_UP);
-		int down = GetAsyncKeyState(VK_DOWN);
-
-		cout << left << right << up << down << endl;
-	}
-	
-	
-
-
 	system("pause");
 	return 0;
 }
